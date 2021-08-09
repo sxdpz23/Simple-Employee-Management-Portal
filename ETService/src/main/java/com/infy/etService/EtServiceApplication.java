20ace3db9097b0253dd140dba3127d84a72d2950
Shantanu Pawar <sxdpz13@gmail.com>
2021-08-09 12:17:20 +0530

    creating essentials

---
+++ ETService/src/main/java/com/infy/etService/EtServiceApplication.java
@@ -0,0 +1,13 @@
+package com.infy.etService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtServiceApplication.class, args);
+	}
+
+}
