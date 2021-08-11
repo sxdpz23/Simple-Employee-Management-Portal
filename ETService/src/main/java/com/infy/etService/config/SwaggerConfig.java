7ad1a47a355c9dec65cb3067fac317b0c9d60e67
Shantanu Pawar <sxdpz13@gmail.com>
2021-08-11 23:58:21 +0530

    Added Actuator Functionality to both server-side apps
    • POM altered
    • Added swagger YAML doc
    • Saved swagger JSON api-doc

--- ETService/src/main/java/com/infy/etService/config/SwaggerConfig.java
+++ ETService/src/main/java/com/infy/etService/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.infy.employeeService.config;
+package com.infy.etService.config;
 
 import org.springframework.context.annotation.Configuration;
 
@@ -10,7 +10,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-/// Link for accessing project swagger-UI ====> http://localhost:3333/e-service/swagger-ui.html
+/// Link for accessing project swagger-UI ====> http://localhost:3334/et-service/swagger-ui.html
 
 @Configuration
 @EnableSwagger2
@@ -18,7 +18,7 @@ public class SwaggerConfig {
 
 	public Docket productApi() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.infy.employeeService.api")).paths(PathSelectors.any())
+				.apis(RequestHandlerSelectors.basePackage("com.infy.etService.api")).paths(PathSelectors.any())
 				.build().apiInfo(metaData());
 	}
 
